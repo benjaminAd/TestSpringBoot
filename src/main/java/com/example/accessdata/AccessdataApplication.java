888dcd0de3d5b8d2425f9866a4d5d126cde9bd95
Benjamin <adolphe906@gmail.com>
2021-01-18 00:02:59 +0100

    Application

---
+++ src/main/java/com/example/accessdata/AccessdataApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessdataApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/accessdata/CityService.java
@@ -0,0 +1,21 @@
+package com.example.accessdata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService implements ICityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Override
+    public List<City> findAll() {
+
+        var cities = (List<City>) repository.findAll();
+
+        return cities;
+    }
+}
